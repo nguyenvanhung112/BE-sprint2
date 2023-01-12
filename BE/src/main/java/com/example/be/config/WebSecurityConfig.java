43a209438fde411bcc929b45e594732d73b72c97
unknown <vanhungns96@gmail.com>
2023-01-12 16:13:51 +0700

    update

--- BE/src/main/java/com/example/be/config/WebSecurityConfig.java
+++ BE/src/main/java/com/example/be/config/WebSecurityConfig.java
@@ -58,14 +58,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/api/auth/**")
-//                .permitAll()
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/user/test2").access("hasRole('ROLE_ADMIN')")
-//                .anyRequest()
-//                .authenticated()
                 .cors().and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint())
                 .and()
--- BE/src/main/java/com/example/be/controller/UserRestController.java
+++ BE/src/main/java/com/example/be/controller/UserRestController.java
@@ -1,4 +1,20 @@
 package com.example.be.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/users")
 public class UserRestController {
+
+    @GetMapping("/test")
+    @Secured({"ROLE_MEMBER"})
+    public String test1() {
+        return "test1";
+    }
+
 }
